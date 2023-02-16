9be5613d4d3bbb15edb41c89b203565a10fb56eb
parkJi-yun <nmjk0123@naver.com>
2023-02-17 05:49:37 +0900

    week3

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,24 @@
+import { useState } from 'react';
+import Category from './components/Category';
+import Restaurants from '../restaurants.json';
+import SearchBer from './components/SearchBer';
+import MenuTable from './components/MenuTable';
+
 export default function App() {
+  const [searchBer, setSearchBer] = useState('');
+  const [category, setCategory] = useState('전체');
+  const [restaurants] = useState(Restaurants.restaurants);
+
   return (
-    <p>
-      과제를 진행해 주세요.
-    </p>
+    <div>
+      <h1>오늘의 메뉴</h1>
+      <SearchBer searchBer={searchBer} setSearchBer={setSearchBer} />
+      <Category restaurants={restaurants} setCategory={setCategory} />
+      <MenuTable
+        restaurants={restaurants}
+        restaurantName={searchBer}
+        category={category}
+      />
+    </div>
   );
 }
---
+++ src/components/MenuTable.tsx
@@ -0,0 +1,54 @@
+import { Restaurant } from '../types/Menu';
+
+type RestaurantsProps = {
+    restaurants: Restaurant[];
+    restaurantName: string;
+    category: string;
+};
+export default function MenuTable({
+  restaurants,
+  restaurantName,
+  category,
+}: RestaurantsProps) {
+  const filteredRestaurants = restaurants
+    .filter((restaurant) => restaurant.name.includes(restaurantName))
+    .filter((restaurant) => {
+      if (category === '전체') return true;
+
+      return restaurant.category === category;
+    });
+
+  return (
+    <div>
+      <table>
+        <thead>
+          <tr>
+            <th>식당 이름</th>
+            <th>종류</th>
+            <th>메뉴</th>
+          </tr>
+        </thead>
+        <tbody>
+          {filteredRestaurants.map((list) => (
+            <tr key={list.id}>
+              <td>{list.name}</td>
+              <td>{list.category}</td>
+              <td>
+                <ul>
+                  {list.menu.map((menu) => (
+                    <li key={menu.id}>
+                      {menu.name}
+                      [
+                      {menu.price}
+                      ]
+                    </li>
+                  ))}
+                </ul>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+}
