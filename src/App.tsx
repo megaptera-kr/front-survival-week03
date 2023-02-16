907424becc83536df2e7ce59d6d251f7e0ecfe9e
heyho00 <heyho9292@gmail.com>
2023-02-17 00:46:23 +0900

    [ADD]전체 data 렌더

--- src/App.tsx
+++ src/App.tsx
@@ -1,58 +1,76 @@
-import restaurants from '../restaurants.json';
+import { ChangeEvent, useState } from 'react';
+import data from '../restaurants.json';
+
+interface Menulist {
+  id:string,
+  name:string,
+  price:number
+}
 
 export default function App() {
-  const onChange = () => null;
+  const [shopName, setShopName] = useState('');
+  const inputShopName = (e: ChangeEvent<HTMLInputElement>) => setShopName(e.target.value);
+  const { restaurants } = data;
+  const categoryNames = ['전체', ...new Set(restaurants.map((shop) => shop.category))];
+
+  const onRenderMenuList = (menu: Menulist[]) => menu.map((item) => (
+    <li key={item.id}>{item.name}</li>
+  ));
+
+  const onRenderRestaurantRow = () => restaurants.map((shop) => {
+    const {
+      id, name, category, menu,
+    } = shop;
+    return (
+      <tr key={id}>
+        <td>{name}</td>
+        <td>{category}</td>
+        <td>
+          <ul>
+            {onRenderMenuList(menu)}
+          </ul>
+        </td>
+      </tr>
+    );
+  });
+
+  const onRenderCategoryBtns = () => categoryNames.map((shop) => (
+    <li key={shop} style={{ marginRight: '1rem' }}>
+      <button type="button">{shop}</button>
+    </li>
+  ));
 
   return (
-    <div>
+    <>
       <h1>오늘의 메뉴</h1>
+
       <div>
         <div>
-          <div>
-            <label htmlFor="input-검색" style={{ paddingRight: '1rem' }}>검색</label>
-            <input id="input-검색" type="text" placeholder="식당 이름" value="" onChange={onChange} />
-          </div>
-          <ul style={{ display: 'flex', padding: 0, listStyle: 'none' }}>
-            <li style={{ marginRight: '1rem' }}>
-              <button type="button">전체</button>
-            </li>
-            <li style={{ marginRight: '1rem' }}>
-              <button type="button">중식</button>
-            </li>
-            <li style={{ marginRight: '1rem' }}>
-              <button type="button">한식</button>
-            </li>
-            <li style={{ marginRight: '1rem' }}>
-              <button type="button">일식</button>
-            </li>
-          </ul>
+          <label htmlFor="input-검색" style={{ paddingRight: '1rem' }}>검색</label>
+          <input id="input-검색" type="text" placeholder="식당 이름" value={shopName} onChange={(e) => inputShopName(e)} />
         </div>
-        <div>
-          <table>
-            <thead>
-              <tr>
-                <th style={{ paddingInline: '2rem' }}>식당 이름</th>
-                <th>종류</th>
-                <th>메뉴</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <td>메가반점</td>
-                <td>중식</td>
-                <td>
-                  <ul>
-                    <li>짜장면(8,000원)</li>
-                    <li>짬뽕(8,000원)</li>
-                    <li>차돌짬뽕(9,000원)</li>
-                    <li>탕수육(14,000원)</li>
-                  </ul>
-                </td>
-              </tr>
-            </tbody>
-          </table>
-        </div>
+
+        <ul style={{ display: 'flex', padding: 0, listStyle: 'none' }}>
+          {onRenderCategoryBtns()}
+        </ul>
+
       </div>
-    </div>
+
+      <div>
+        <table>
+          <thead>
+            <tr>
+              <th style={{ paddingInline: '2rem' }}>식당 이름</th>
+              <th>종류</th>
+              <th>메뉴</th>
+            </tr>
+
+          </thead>
+          <tbody>
+            {onRenderRestaurantRow()}
+          </tbody>
+        </table>
+      </div>
+    </>
   );
 }
