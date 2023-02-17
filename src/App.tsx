a5ea95d610cd25455ba2216a525323c0a999b662
Jeon Minji <mjjeon2645@gmail.com>
2023-02-17 10:15:44 +0900

    Add: 페이지 구분해보기(섹션 나누기)
    완성 페이지의 layout을 구분해봄(아토믹 디자인의 template 정도)

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
 export default function App() {
   return (
-    <p>
-      과제를 진행해 주세요.
-    </p>
+    <div>
+      <div>
+        오늘의 메뉴 headline
+      </div>
+      <div>
+        검색 input
+      </div>
+      <div>
+        필터링 button
+      </div>
+      <div>
+        테이블 영역
+      </div>
+    </div>
   );
 }
