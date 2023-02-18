d07179d0118aaba875bf7e9a006f4b99c04393ca
BORA <a52447879@gmail.com>
2023-02-18 17:01:57 +0900

    feat: filterable-table 전체적인 구조 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,50 @@
+import data from '../restaurants.json';
+
 export default function App() {
   return (
-    <p>
-      과제를 진행해 주세요.
-    </p>
+    <div className="filterable-table">
+      <h1>오늘의 메뉴</h1>
+
+      <div className="search-bar">
+        <label htmlFor="restaurant-name">검색</label>
+        <input type="text" id="restaurant-name" />
+      </div>
+
+      <div className="filter-action-button">
+        <button type="button">전체</button>
+        <button type="button">중식</button>
+        <button type="button">한식</button>
+        <button type="button">일식</button>
+      </div>
+
+      <table>
+        <thead>
+          <tr>
+            <th>식당 이름</th>
+            <th>종류</th>
+            <th>메뉴</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.restaurants.map((restaurant) => (
+            <tr key={restaurant.id}>
+              <td>{restaurant.name}</td>
+              <td>{restaurant.category}</td>
+              <td>
+                {
+                  restaurant.menu.map((menuItem) => (
+                    <ul key={menuItem.id}>
+                      <li>{`${menuItem.name}(${menuItem.price})`}</li>
+                    </ul>
+
+                  ))
+                }
+              </td>
+            </tr>
+          ))}
+
+        </tbody>
+      </table>
+    </div>
   );
 }
