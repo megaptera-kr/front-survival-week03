dbdfd66bbed27d04edb8775dd203cd6b1912dfda
BORA <a52447879@gmail.com>
2023-02-18 22:51:32 +0900

    feat: Toolbar 분리, FilterCategoryButtonGroup 추가, 공통 버튼 컴포넌트 추가

---
+++ src/components/Button.tsx
@@ -0,0 +1,11 @@
+type ButtonProps = {
+  label: string
+}
+
+function Button({ label }: ButtonProps) {
+  return (
+    <button type="button">{label}</button>
+  );
+}
+
+export default Button;
---
+++ src/components/FilterCategoryButtonGroup/index.tsx
@@ -0,0 +1,32 @@
+import Button from '../Button';
+
+const CATEGORY_BUTTON_MAP = [{
+  id: 10,
+  label: '전체',
+}, {
+  id: 20,
+  label: '중식',
+}, {
+  id: 30,
+  label: '한식',
+}, {
+  id: 40,
+  label: '일식',
+}];
+
+const buttonStyles = {
+  display: 'flex',
+  gap: '16px',
+};
+
+function FilterCategoryButtonGroup() {
+  return (
+    <div className="filter-action-button" style={buttonStyles}>
+      {CATEGORY_BUTTON_MAP.map((button) => (
+        <Button key={button.id} label={button.label} />
+      ))}
+    </div>
+  );
+}
+
+export default FilterCategoryButtonGroup;
