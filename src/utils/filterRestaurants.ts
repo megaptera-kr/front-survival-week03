1f729da4f7a0eb707105f0f1640b882cf1c12ed6
BORA <a52447879@gmail.com>
2023-02-19 02:20:16 +0900

    refactor(utils): filterRestaurants 함수 (사용자 입력에 따른 필터링 구현 함수) 수정

--- src/utils/filterRestaurants.ts
+++ src/utils/filterRestaurants.ts
@@ -1,6 +1,16 @@
 import { Restaurants } from '../types/filterableProductTable';
 
-function filterRestaurants(items: Restaurants[], category: string) {
-  return items.filter((item) => item.category === category);
+function normalize(text: string) {
+  return text.trim();
+}
+
+function filterRestaurants(items: Restaurants[], filterText: string) {
+  const query = normalize(filterText);
+
+  if (!query) return items;
+
+  const contains = (item:Restaurants) => item.category.includes(query);
+
+  return items.filter(contains);
 }
 export default filterRestaurants;
