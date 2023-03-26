adb860edecacb17cc28d583cf8cd45e1a815e7e6
StarryPro <hansunguk6178@gmail.com>
2023-03-26 21:08:00 +0900

    :sparkles: feat: App 및 main 파일 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,33 @@
+import { useState } from 'react';
+
+import RestaurantTable from './components/RestaurantTable';
+import SearchBar from './components/SearchBar';
+
+import filterRestaurants from './utils/filterRestaurant';
+import selectCategories from './utils/selectCategories';
+
+import { restaurants } from '../restaurants.json';
+
 export default function App() {
+  const [filterText, setFilterText] = useState<string>('');
+  const [filterCategory, setFilterCategory] = useState<string>('전체');
+
+  const filterRestaurant = filterRestaurants(restaurants, {
+    filterText,
+    filterCategory,
+  });
+
+  const categories = selectCategories(restaurants);
   return (
-    <p>
-      과제를 진행해 주세요.
-    </p>
+    <>
+      <h1>오늘의 메뉴</h1>
+      <SearchBar
+        filterText={filterText}
+        setFilterText={setFilterText}
+        setFilterCategory={setFilterCategory}
+        categories={categories}
+      />
+      <RestaurantTable restaurants={filterRestaurant} />
+    </>
   );
 }
