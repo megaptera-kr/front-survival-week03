105ca8dae2cb28bf18c5b52f8b75e34dc10671dc
jtwjs <xodnd9503@gmail.com>
2023-03-27 17:12:35 +0900

    feat: 불필요 코드 제거

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,8 @@
-import { useState } from 'react';
-
 import FilterableRestaurantTable from './components/filterableRestaurantTable/FilterableRestaurantTable';
-
-import { RestaurantFilter } from './types/restaurants';
 
 import { restaurants } from '../restaurants.json';
 
 export default function App() {
-  const [filter, setFilter] = useState<RestaurantFilter>({
-    searchText: '',
-    category: '',
-  });
-
-  const handleChangeSearchText = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const { value } = e.target;
-    setFilter((prev) => ({ ...prev, searchText: value.trim() }));
-  };
-
-  const handleClickCategoryFilter = (ctg: string) => () => {
-    setFilter((prev) => ({ ...prev, category: ctg }));
-  };
-
   return (
     <>
       <h1>오늘의 메뉴</h1>
