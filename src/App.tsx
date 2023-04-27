8dc2730fa31ef8cc03d056ff7459c2ca578db448
lswon2885 <lswon2885@daum.net>
2023-04-27 15:36:44 +0900

    식당이름 노출

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,48 @@
+import food from '../restaurants.json';
+
 export default function App() {
   return (
-    <p>
-      과제를 진행해 주세요.
-    </p>
+    <div>
+      <h1>오늘의 메뉴</h1>
+      <div>
+        <label htmlFor="input">검색</label>
+        <input type="text" placeholder="식당 이름" />
+        <ul>
+          <button type="button">전체</button>
+          <button type="button">중식</button>
+          <button type="button">한식</button>
+          <button type="button">일식</button>
+        </ul>
+      </div>
+      <table>
+        <thead>
+          <tr>
+            <th>식당이름</th>
+            <th>종류</th>
+            <th>메뉴</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>{food.restaurants[0].name}</td>
+          </tr>
+          <tr>
+            <td>{food.restaurants[1].name}</td>
+          </tr>
+          <tr>
+            <td>{food.restaurants[2].name}</td>
+          </tr>
+          <tr>
+            <td>{food.restaurants[3].name}</td>
+          </tr>
+          <tr>
+            <td>{food.restaurants[4].name}</td>
+          </tr>
+          <tr>
+            <td>{food.restaurants[5].name}</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
   );
 }
