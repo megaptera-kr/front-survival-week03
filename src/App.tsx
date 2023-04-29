3a9d2f0a084f078a9c7b6ed4c7dc67b3c9ba75c1
OD <kg-te@naver.com>
2023-04-30 01:08:03 +0900

    ✨ App 컴포넌트 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,28 @@
+import { useState } from 'react';
+import FilterButton from './components/FilterButton';
+import ProductTable from './components/ProductTable';
+import SearchBar from './components/SearchBar';
+import Title from './components/Title';
+import restaurantsList from '../restaurants.json';
+import FILTER_LIST from './constants/constants';
+
 export default function App() {
+  const { restaurants }: { restaurants: Restaurant[] } = restaurantsList;
+
+  type FilterType = (typeof FILTER_LIST)[number];
+  const [filterType, setFilterType] = useState<FilterType>('전체');
+  const [keyword, setKeyword] = useState<string>('');
+
   return (
-    <p>
-      과제를 진행해 주세요.
-    </p>
+    <>
+      <Title />
+      <SearchBar setKeyword={setKeyword} />
+      <FilterButton data={FILTER_LIST} setFilterType={setFilterType} />
+      <ProductTable
+        data={restaurants}
+        filterType={filterType}
+        keyword={keyword}
+      />
+    </>
   );
 }
