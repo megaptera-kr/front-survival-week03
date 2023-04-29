95f6736dafc915bee417425a8c3f49792db70552
OD <kg-te@naver.com>
2023-04-30 01:03:57 +0900

    🏷️ 타입 추가

---
+++ src/types/restaurants.ts
@@ -0,0 +1,16 @@
+type Menu = {
+  id: string;
+  name: string;
+  price: number;
+};
+
+type Restaurant = {
+  id: string;
+  category: string;
+  name: string;
+  menu: Menu[];
+};
+
+type RestaurantList = {
+  restaurants: Restaurant[];
+};
