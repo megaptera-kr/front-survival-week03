1ac9d1be278cea141d19203f3b7280b7893d38f2
dico1631 <dicoqnfkrzl@gmail.com>
2023-05-07 13:06:38 +0900

    setBtn

--- src/components/ButtonField.tsx
+++ src/components/ButtonField.tsx
@@ -1,10 +1,18 @@
+import { useState } from "react";
+
 type ButtonFieldProps = {
 	value: string;
 };
 
 export default function ButtonField({ value }: ButtonFieldProps) {
+	const [btnValue, setBtnValue] = useState(value);
+
+	const handleClick = (e) => {
+		setBtnValue(e.target.value);
+		console.log(btnValue);
+	}
 
 	return (
-		<button type='button' value={value}>{value}</button>
+		<button type='button' value={btnValue} onClick={handleClick}>{btnValue}</button>
 	);
 }
