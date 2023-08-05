c8e665c6522867cc0b75e6e20faec1274cb82141
bereal1995 <gygud98@gmail.com>
2023-08-05 21:58:10 +0900

    feat: 비어있는 경우 테스트케이스 수정

--- src/components/RestaurantsTable.test.tsx
+++ src/components/RestaurantsTable.test.tsx
@@ -6,10 +6,11 @@ import { restaurants as restaurantsData } from '../../restaurants.json';
 
 describe('RestaurantsTable', () => {
   describe('restaurants가 없는 경우', () => {
-    it('renders "텅~" text', () => {
+    it('화면에 리스트를 노출하지 않는다.', () => {
       const { container } = render(<RestaurantsTable restaurants={[]} />);
+      const tbody = container.querySelector('tbody');
 
-      expect(container).toHaveTextContent('텅~');
+      expect(tbody).toBeEmptyDOMElement();
     });
   });
 
