9312247d265c8d7d033b6b2d0cc75f1deb6af2da
jiwon0995 <jiwon0995@gmail.com>
2023-08-06 01:02:50 +0900

    fix: main.tsx fix

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,15 @@
-import ReactDOM from 'react-dom';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 import App from './App';
 
 function main() {
-  // TODO: App 컴포넌트를 render 해주세요.
-  ReactDOM.render(<App />, document.getElementById('root'));
+  const container = document.getElementById('root');
+  if (!container) {
+    return;
+  }
+
+  const root = ReactDOM.createRoot(container);
+  root.render(React.createElement(App, null));
 }
 
 main();
