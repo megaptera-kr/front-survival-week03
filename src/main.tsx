f236e66f6d6f74491a296b9d7b11ee11d6f3e87b
canonmj <canon.minjoo@gmail.com>
2023-08-06 17:58:49 +0900

    first commit

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+
 function main() {
+  const element = document.getElementById('root');
+  if (!element) {
+    return;
+  }
+  const root = ReactDOM.createRoot(element);
+  root.render(<App />);
   // TODO: App 컴포넌트를 render 해주세요.
 }
 
