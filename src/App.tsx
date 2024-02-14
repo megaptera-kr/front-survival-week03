00a1c5310b5eb861ebce0ba6c1a10bdebc8f881d
swd <tladnjsen@gmail.com>
2024-02-14 16:19:11 +0900

    Add innter component FilteredMenuTable

--- src/App.tsx
+++ src/App.tsx
@@ -86,6 +86,40 @@ const restaurantData: RestaurantList = {
   ],
 };
 
+function FilteredMenuTable({ restaurants }: RestaurantList) {
+  return (
+    <>
+      <table className='menu-table'>
+        <thead>
+          <tr>
+            <th>식당 이름</th>
+            <th>종류</th>
+            <th>메뉴</th>
+          </tr>
+        </thead>
+        <tbody>
+          {restaurants.map((restaurant) => (
+            <tr key={restaurant.id}>
+              <td>{restaurant.name}</td>
+              <td>{restaurant.category}</td>
+
+              <td>
+                {restaurant.menu.map((item) => (
+                  <ul key={item.id}>
+                    <li>
+                      {item.name}({item.price})
+                    </li>
+                  </ul>
+                ))}
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </>
+  );
+}
+
 export default function App() {
   return (
     <div className='filtered-menu-container'>
@@ -101,66 +135,7 @@ export default function App() {
           <button type='button'>한식</button>
           <button type='button'>일식</button>
         </div>
-
-        <table className='menu-table'>
-          <thead>
-            <tr>
-              <th>식당 이름</th>
-              <th>종류</th>
-              <th>메뉴</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td>메가 반점</td>
-              <td>중식</td>
-              <td>
-                <ul>
-                  <li>짜장면(8,000원)</li>
-                  <li>짬뽕(8,000원)</li>
-                  <li>차돌짬뽕(8,000원)</li>
-                  <li>탕수육(8,000원)</li>
-                </ul>
-              </td>
-            </tr>
-            <tr>
-              <td>메가 반점</td>
-              <td>중식</td>
-              <td>
-                <ul>
-                  <li>짜장면(8,000원)</li>
-                  <li>짬뽕(8,000원)</li>
-                  <li>차돌짬뽕(9,000원)</li>
-                  <li>탕수육(14,000원)</li>
-                </ul>
-              </td>
-            </tr>
-            <tr>
-              <td>메리 김밥</td>
-              <td>한식</td>
-              <td>
-                <ul>
-                  <li>김밥(3,500원)</li>
-                  <li>참치김밥(4,500원)</li>
-                  <li>제육김밥(5,000원)</li>
-                  <li>컵라면(2,000원)</li>
-                </ul>
-              </td>
-            </tr>
-            <tr>
-              <td>데브부엌</td>
-              <td>한식</td>
-              <td>
-                <ul>
-                  <li>제육덮밥(10,000원)</li>
-                  <li>닭떡국(9,000원)</li>
-                  <li>닭개장(8,000원)</li>
-                  <li>돈가쓰(7,000원)</li>
-                </ul>
-              </td>
-            </tr>
-          </tbody>
-        </table>
+        <FilteredMenuTable restaurants={restaurantData.restaurants} />
       </section>
     </div>
   );
