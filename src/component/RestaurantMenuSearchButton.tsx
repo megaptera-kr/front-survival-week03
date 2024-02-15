47255a1bd91018ca8e24a5757b6d9a3fb07a8e43
swd <tladnjsen@gmail.com>
2024-02-15 16:22:53 +0900

    Apply css to clicked button

--- src/component/RestaurantMenuSearchButton.tsx
+++ src/component/RestaurantMenuSearchButton.tsx
@@ -14,9 +14,19 @@ export default function RestaurantMenuSearchButton({
     handleButton(category);
   };
 
+  const styleButton = {
+    backgroundColor: text === category ? 'yellow' : '',
+    marginRight: '1rem',
+  };
+
   return (
     <>
-      <button type='button' value={text} onClick={hanleOnClick}>
+      <button
+        type='button'
+        value={text}
+        style={styleButton}
+        onClick={hanleOnClick}
+      >
         {text || '전체 '}
       </button>
     </>
