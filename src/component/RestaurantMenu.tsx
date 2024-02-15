28ed685f770c75ddd2425b85021d3ce2afbc45ef
swd <tladnjsen@gmail.com>
2024-02-15 14:43:06 +0900

    Modify price display

--- src/component/RestaurantMenu.tsx
+++ src/component/RestaurantMenu.tsx
@@ -5,11 +5,14 @@ type RestaurantMenuProps = {
 };
 
 export default function RestaurantMenu({ menu }: RestaurantMenuProps) {
+  const { id, name, price } = menu;
+  const formattedPrice = new Intl.NumberFormat().format(price);
+
   return (
     <>
-      <ul key={menu.id}>
+      <ul key={id}>
         <li>
-          {menu.name}({menu.price})
+          {name}({formattedPrice}원)
         </li>
       </ul>
     </>
