edf203f0f178050dc859fc95a54fd4c4a8a6dcea
Juhwan <dev.juhwanlee@gmail.com>
2024-02-17 19:05:21 +0900

    Feat: implement restaurant filtering and search functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,13 @@
-/**
-오늘의 메뉴를 고를 수 있도록 아래 조건에 따라 식당 목록을 볼 수 있도록 만들어 주세요.
-
-- 사용자는 식당 이름, 종류, 메뉴가 보이는 식당 목록을 볼 수 있다.
-- 사용자는 식당 이름을 입력하여 이름이 (부분)일치하는 식당 목록을 골라 볼 수 있다.
-- 사용자는 식당 종류 버튼을 눌러서 종류가 일치하는 식당 목록만 골라 볼 수 있다.
-- 사용자는 입력한 식당 이름과 선택한 종류가 모두 일치하는 식당 목록만 골라 볼 수 있다.
- */
+import { useState } from 'react';
 import jsonData from '../restaurants.json';
 import './globals.css';
 
 export default function App() {
+  const [filter, setFilter] = useState({
+    name: '',
+    category: '전체',
+  });
+
   const { restaurants } = jsonData;
   const foodCategories = restaurants.reduce<string[]>(
     (acc, cur) => {
@@ -21,13 +19,32 @@ export default function App() {
     , ['전체'],
   );
 
+  const filteredRestaurants = restaurants.filter((restaurant) => {
+    const nameFilter = filter.name.trim() === '' || restaurant.name.includes(filter.name);
+    const categoryFilter = filter.category === '전체' || restaurant.category === filter.category;
+    return nameFilter && categoryFilter;
+  });
+
+  const handleFilterChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setFilter({
+      ...filter,
+      name: e.target.value.trim(),
+    });
+  };
+  const handleClickCategory = (category: string) => {
+    setFilter({
+      ...filter,
+      category,
+    });
+  };
+
   return (
     <>
       <h1>오늘의 메뉴</h1>
 
       <label style={{ display: 'flex', gap: '1rem' }}>
         <p>검색</p>
-        <input type="text" placeholder="식당 이름을 검색하세요" />
+        <input type="text" maxLength={20} placeholder="식당 이름을 검색하세요" onChange={(e) => handleFilterChange(e)} />
       </label>
 
       <ul style={{
@@ -36,7 +53,7 @@ export default function App() {
       >
         {foodCategories.map((category) => (
           <li key={category}>
-            <button type="button" key={category}>{category}</button>
+            <button type="button" key={category} style={{ backgroundColor: filter.category === category ? 'gray' : undefined }} onClick={() => handleClickCategory(category)}>{category}</button>
           </li>
         ))}
       </ul>
@@ -49,7 +66,7 @@ export default function App() {
           </tr>
         </thead>
         <tbody>
-          {restaurants.map((restaurant) => (
+          {filteredRestaurants.map((restaurant) => (
             <tr key={restaurant.id}>
               <td>{restaurant.name}</td>
               <td>{restaurant.category}</td>
