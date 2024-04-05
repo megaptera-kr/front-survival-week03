fc5472e3f721e3bc80b0e87dc048468813ea9cd1
WATA-Yiju <yiju.dev@watanow.com>
2024-04-05 09:07:54 +0900

    feat: App 컴포넌트 정적인 버전 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,115 @@
+import RestaurantRow from './components/RestaurantRow';
+import { Restaurant } from './types/Restaurant';
+
+const restaurants:Restaurant[] = [
+  {
+    id: '1',
+    category: '중식',
+    name: '메가반점',
+    menu: [
+      { id: '1', name: '짜장면', price: 8000 },
+      { id: '2', name: '짬뽕', price: 8000 },
+      { id: '3', name: '차돌짬뽕', price: 9000 },
+      { id: '4', name: '탕수육', price: 14000 },
+    ],
+  },
+  {
+    id: '2',
+    category: '한식',
+    name: '메리김밥',
+    menu: [
+      { id: '5', name: '김밥', price: 3500 },
+      { id: '6', name: '참치김밥', price: 4500 },
+      { id: '7', name: '제육김밥', price: 5000 },
+      { id: '8', name: '훈제오리김밥', price: 5500 },
+      { id: '9', name: '컵라면', price: 2000 },
+    ],
+  },
+  {
+    id: '3',
+    category: '한식',
+    name: '데브부엌',
+    menu: [
+      { id: '10', name: '제육덮밥', price: 10000 },
+      { id: '11', name: '닭떡국', price: 9000 },
+      { id: '12', name: '닭개장', price: 11000 },
+      { id: '13', name: '돈까스', price: 1000 },
+    ],
+  },
+  {
+    id: '4',
+    category: '일식',
+    name: '로드스시',
+    menu: [
+      { id: '14', name: '모듬초밥', price: 14000 },
+      { id: '15', name: '특선초밥', price: 17900 },
+      { id: '16', name: '스페셜초밥', price: 21000 },
+    ],
+  },
+  {
+    id: '5',
+    category: '일식',
+    name: '혹등고래카레',
+    menu: [
+      { id: '17', name: '기본카레', price: 9000 },
+      { id: '18', name: '가라아게카레', price: 14000 },
+      { id: '19', name: '소시지카레', price: 13000 },
+      { id: '20', name: '돈까스카레', price: 14000 },
+      { id: '21', name: '닭가슴살카레', price: 13000 },
+    ],
+  },
+  {
+    id: '6',
+    category: '한식',
+    name: '메가김치찌개',
+    menu: [
+      { id: '22', name: '김치찌개1인', price: 8000 },
+      { id: '23', name: '된장찌개', price: 8000 },
+      { id: '24', name: '계란말이', price: 6000 },
+    ],
+  },
+];
+
 export default function App() {
+  const categories = restaurants.reduce(
+    (acc: string[], restaurant) => (acc.includes(restaurant.category)
+      ? acc : [...acc, restaurant.category])
+    , [],
+  );
+
   return (
-    <p>
-      과제를 진행해 주세요.
-    </p>
+    <div>
+      <h1>오늘의 메뉴</h1>
+
+      <div>
+        <label htmlFor="search">검색</label>
+        <input type="text" id="search" placeholder="식당 이름" />
+      </div>
+
+      <div>
+        <button type="button">전체</button>
+        {categories.map((category) => (
+          <button key={category} type="button">{category}</button>
+        ))}
+      </div>
+
+      <table>
+        <thead>
+          <tr>
+            <th>식당 이름</th>
+            <th>종류</th>
+            <th>메뉴</th>
+          </tr>
+        </thead>
+        <tbody>
+          {restaurants.map((restaurant) => (
+            <RestaurantRow
+              key={restaurant.id}
+              restaurant={restaurant}
+            />
+          ))}
+        </tbody>
+      </table>
+    </div>
   );
 }
