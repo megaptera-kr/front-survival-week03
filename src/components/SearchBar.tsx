82e22a469c1d063a237c0d5d378dbb3e8d7220bb
Han Jung Ah <truth.h406@gmail.com>
2024-04-10 23:24:20 +0900

    linting

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -13,8 +13,8 @@ export default function SearchBar({
   return (
     <div>
       <TextField
-        label='검색'
-        placeholder='식당 이름'
+        label="검색"
+        placeholder="식당 이름"
         filterText={filterText}
         setFilterText={setFilterText}
       />
