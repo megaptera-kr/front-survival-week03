00c7828f7936b1f4509f726533a9fbc46b9b4f4b
ain <lovepizza132@gmail.com>
2024-06-15 14:33:31 +0900

    feat App및 type 생성

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,100 @@
+import { restaurants } from '../restaurants.json';
+import Restaurant from './type/Restaurant';
+
 export default function App() {
+  const categories = restaurants.reduce((acc: string[], restaurant: Restaurant) => {
+    const { category } = restaurant;
+    return acc.includes(category) ? acc : [...acc, category];
+  }, []);
+
   return (
-    <p>
-      과제를 진행해 주세요.
-    </p>
+    <div className="filterable-restaurant-table">
+      <h1>오늘의 메뉴</h1>
+      <div>
+        <div className="search-bar">
+          <div>
+            <label
+              htmlFor="input-검색"
+              style={{
+                paddingRight: '1rem',
+              }}
+            >
+              검색
+            </label>
+            <input
+              id="input-검색"
+              type="text"
+              placeholder="식당 이름"
+            />
+          </div>
+          <ul
+            style={{
+              listStyle: 'none',
+              display: 'flex',
+              padding: 0,
+            }}
+          >
+            {['전체', ...categories].map((category) => (
+              <li
+                key={category}
+                style={{
+                  paddingRight: '1rem',
+                }}
+              >
+                <button
+                  type="button"
+                >
+                  {category}
+                </button>
+              </li>
+            ))}
+          </ul>
+        </div>
+        <div className="restaurants">
+          <table>
+            <thead>
+              <tr>
+                <th
+                  style={{
+                    paddingInline: '2rem',
+                  }}
+                >
+                  식당 이름
+                </th>
+                <th>종류</th>
+                <th>메뉴</th>
+              </tr>
+            </thead>
+            <tbody>
+              {restaurants.map((restaurant) => (
+                <tr
+                  key={restaurant.id}
+                >
+                  <td>{restaurant.name}</td>
+                  <td>{restaurant.category}</td>
+                  <td>
+                    {restaurant.menu.map((food) => (
+                      <ul
+                        key={food.id}
+                        style={{
+                          margin: 0,
+                        }}
+                      >
+                        <li>
+                          {food.name}
+                          (
+                          {food.price}
+                          원)
+                        </li>
+                      </ul>
+                    ))}
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,14 @@
+import ReactDom from 'react-dom/client';
+import App from './App';
+
 function main() {
-  // TODO: App 컴포넌트를 render 해주세요.
+  const container = document.getElementById('root');
+  if (!container) {
+    return;
+  }
+
+  const root = ReactDom.createRoot(container);
+  root.render(<App />);
 }
 
 main();
